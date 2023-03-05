dc9049fb378d0bcb78cd49536c8de1b3e8e71b5a
Anton Rychagov <anton.rychagov@gmail.com>
2023-03-05 19:04:14 +0400

    Total rebalance of Adeptus Mechanicus upgrades tree

---
+++ mvbeta.py
@@ -0,0 +1,10 @@
+import shutil
+
+MOD_DATA_PATH = u'C:\\Users\\anton\\OneDrive\\Documents\\Proxy Studios\\Gladius\\Mods\\Adeptus Beta\\Data'
+DEV_MOD_PATH = u'C:\\Adeptus\\Mod\\Data'
+
+try:
+    shutil.rmtree(MOD_DATA_PATH)
+except:
+    pass
+shutil.copytree(DEV_MOD_PATH, MOD_DATA_PATH)
