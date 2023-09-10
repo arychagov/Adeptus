ff74ab0bb1df4b7cdddb25949273f6a23f031884
Anton Rychagov <anton.rychagov@gmail.com>
2023-09-10 14:24:16 +0400

    Reworked Cryptek

--- mv.py
+++ mv.py
@@ -1,10 +1,15 @@
 import shutil
 
-MOD_DATA_PATH = u'C:\\Users\\anton\\OneDrive\\Documents\\Proxy Studios\\Gladius\\Mods\\Adeptus\\Data'
+STABLE_MOD_DATA_PATH = u'C:\\Users\\anton\\OneDrive\\Documents\\Proxy Studios\\Gladius\\Mods\\Adeptus\\Data'
+BETA_MOD_DATA_PATH = u'C:\\Users\\anton\\OneDrive\\Documents\\Proxy Studios\\Gladius\\Mods\\Adeptus Beta\\Data'
 DEV_MOD_PATH = u'C:\\Adeptus\\Mod\\Data'
 
-try:
-    shutil.rmtree(MOD_DATA_PATH)
-except:
-    pass
-shutil.copytree(DEV_MOD_PATH, MOD_DATA_PATH)
+def delete_and_copy(mod_path):
+    try:
+        shutil.rmtree(mod_path)
+        shutil.copytree(DEV_MOD_PATH, mod_path)
+    except:
+        pass
+
+delete_and_copy(STABLE_MOD_DATA_PATH)
+delete_and_copy(BETA_MOD_DATA_PATH)
