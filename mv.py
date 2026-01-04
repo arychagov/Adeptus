ae0da58935cc7f8c5b9f9b9c917a9099ea3d8e93
Wealduun <wealduun@gmail.com>
2026-01-04 10:48:10 +0400

    fine tune

--- mv.py
+++ mv.py
@@ -4,12 +4,14 @@ STABLE_MOD_DATA_PATH = u'C:\\Users\\anton\\OneDrive\\Documents\\Proxy Studios\\G
 BETA_MOD_DATA_PATH = u'C:\\Users\\anton\\OneDrive\\Documents\\Proxy Studios\\Gladius\\Mods\\Adeptus Beta\\Data'
 DEV_MOD_PATH = u'C:\\Adeptus\\Mod\\Data'
 
+
 def delete_and_copy(mod_path):
     try:
         shutil.rmtree(mod_path)
         shutil.copytree(DEV_MOD_PATH, mod_path)
     except:
         pass
+
 
 delete_and_copy(STABLE_MOD_DATA_PATH)
 delete_and_copy(BETA_MOD_DATA_PATH)
